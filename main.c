b29228fc5c0ec2c7db06c4fc258aeac2036d7247
256k <fayek.helmi@gmail.com>
2023-09-11 00:46:09 -0400

    small formatting with emacs

--- main.c
+++ main.c
@@ -4,8 +4,8 @@
 
 // type definitions
 struct entry {
-	char *label;
-	struct tm date;
+  char *label;
+  struct tm date;
 };
 
 #define MAX_LABEL_LENGTH 60
@@ -16,60 +16,55 @@ int list_length = 0;
 
 // function definitions
 void welcome_message() {
-	printf("\n");
-	printf("\n");
-	printf("X days since Y\n");
-	printf("==============\n");
-	printf("\n");
+  printf("\n");
+  printf("\n");
+  printf("X days since Y\n");
+  printf("==============\n");
+  printf("\n");
 };
 
 struct tm getDate() {
-	time_t t = time(NULL);
-	struct tm tm = *localtime(&t);
-	return tm;
+  time_t t = time(NULL);
+  struct tm tm = *localtime(&t);
+  return tm;
 };
 
 void new_entry(char* entry_label, struct entry* list, int* list_length) {
-	struct entry temp_entry;
-	temp_entry.label = entry_label;
-	temp_entry.date = getDate();
-	list[*list_length] = temp_entry;
-	*list_length = *list_length + 1;
+  struct entry temp_entry;
+  temp_entry.label = entry_label;
+  temp_entry.date = getDate();
+  list[*list_length] = temp_entry;
+  *list_length = *list_length + 1;
 }; 
 
 void print_list(struct entry* list, int* list_length) {
-	printf("==============\n");
-	for (int i = 0; i < *list_length; i++) {
-		printf("[%d] days since ", i);
-		printf("%s", list[i].label);
-	}
-	printf("==============\n");
+  printf("==============\n");
+  for (int i = 0; i < *list_length; i++) {
+    printf("[%d] days since ", i);
+    printf("%s", list[i].label);
+  }
+  printf("==============\n");
 };
 
 void prompt_user() {
-	char input_text[MAX_LABEL_LENGTH ];
-	printf("> ");
-	fgets(input_text, MAX_LABEL_LENGTH , stdin);
-	printf("added: %s\n", input_text);
-	if (input_text[0] == '/') {
-		// run command function
-		print_list(list, &list_length);
-	} else {
-		new_entry(input_text, list, &list_length);
-	}
-	prompt_user();
+  char input_text[MAX_LABEL_LENGTH ];
+  printf("> ");
+  fgets(input_text, MAX_LABEL_LENGTH , stdin);
+  printf("added: %s\n", input_text);
+  if (input_text[0] == '/') {
+    // run command function
+    print_list(list, &list_length);
+  } else {
+    new_entry(input_text, list, &list_length);
+  }
+  prompt_user();
 };
 
 
 int main(int argc, char *argv[]) {
-
-	welcome_message();
-	prompt_user();
+  welcome_message();
+  prompt_user();
 };
-
-
-
-
 
 
   // time_t t = time(NULL);
