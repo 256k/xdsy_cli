3cac8efeb1d493d65e0d756868cbe39bbeada6cb
256k <fayek.helmi@gmail.com>
2023-09-10 23:02:46 -0400

    switched scanf for fgets and added ignore files

--- main.c
+++ main.c
@@ -14,6 +14,8 @@ int list_length = 0;
 
 // function definitions
 void welcome_message() {
+	printf("\n");
+	printf("\n");
 	printf("X days since Y\n");
 	printf("==============\n");
 	printf("\n");
@@ -26,7 +28,6 @@ struct tm getDate() {
 };
 
 void new_entry(char* entry_label, struct entry* list, int* list_length) {
-	printf("%s\n", entry_label);
 	struct entry temp_entry;
 	temp_entry.label = entry_label;
 	temp_entry.date = getDate();
@@ -34,32 +35,33 @@ void new_entry(char* entry_label, struct entry* list, int* list_length) {
 	*list_length = *list_length + 1;
 }; 
 
-void prompt_user() {
-	char input_text[40];
-	printf("> ");
-	scanf("%[^\n]", input_text);
-	if (input_text[0] == '/') {
-		// run command function
-		printf("run command function");
-	} else {
-		printf("run new entry function");
-		// run new entry function
-		new_entry(input_text, list, &list_length);
-		printf("saved!\n");
-	}
-};
-
 void print_list(struct entry* list, int* list_length) {
 	printf("==============\n");
-	for (int i = 0; i <= *list_length; i++) {
+	for (int i = 0; i < *list_length; i++) {
 		printf("[%d] days since ", i);
 		printf("%s\n", list[i].label);
 	}
 	printf("==============\n");
 };
 
+void prompt_user() {
+	char input_text[40];
+	printf("> ");
+	fgets(input_text, 40, stdin);
+		printf("input_text: %s\n", input_text);
+	if (input_text[0] == '/') {
+		// run command function
+		print_list(list, &list_length);
+	} else {
+		// printf("run new entry function\n");
+		// run new entry function
+		new_entry(input_text, list, &list_length);
+	}
+	prompt_user();
+};
+
+
 int main(int argc, char *argv[]) {
-	printf("list length: %d\n", list_length);
 
 	char input_text[40];
 	welcome_message();
